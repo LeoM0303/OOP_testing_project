065310b5cd74de2caf6fd9ed1fd229bbb3c0219b
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-06 13:27:09 +0200

    Add parameters to set_coords method and update usage
    The set_coords method now accepts x and y as parameters to set point coordinates. Updated the method call and added an example printing the instance dictionary to verify changes. This improves functionality and allows more flexibility in setting coordinates.

--- course/1.py
+++ course/1.py
@@ -2,6 +2,10 @@ class Point:
     color = 'red'
     circle = 2
 
-    def set_coords(self):
-        print('Set coords' + str(self))
+    def set_coords(self, x,y):
+        self.x = x
+        self.y = y
 
+pt = Point()
+pt.set_coords(1,2)
+print(pt.__dict__)

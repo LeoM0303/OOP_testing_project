b258eebfe2a5f4d8ba7d88598e922fa71bb8a2dd
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-06 17:44:15 +0200

    Refactor: Replace Point class with DataBase class implementation
    Introduced a new DataBase class to handle user connection logic, including methods for connecting, closing, reading, and writing data. Removed the unused Point class to streamline the code and focus on database operations.

--- course/3.py
+++ course/3.py
@@ -1,12 +1,17 @@
-class Point:
-    def __new__(cls, *args, **kwargs):
-        print('__new__ called' + str(cls))
-        return super().__new__(cls)
+class DataBase:
+    def __init__(self):
+        self.user = user()
+        self.psw = psw
+        self.port = port
 
-    def __init__(self, x=0, y=0):
-        print('__init__ called' + str(self))
-        self.x = x
-        self.y = y
+    def connect(self):
+        print(f'Conect with DataBase: user: {self.user} psw: {self.psw} port: {self.port}')
 
-pt = Point(1,2)
-print(pt)
+    def close(self):
+        print('Close connection')
+
+    def read(self):
+        print('Read data')
+
+    def write(self):
+        print(f'Write in data {data}')

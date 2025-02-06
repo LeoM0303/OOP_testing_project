da87c29e3053b37d7c3094483dc4c8c582aa38fb
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-06 14:40:36 +0200

    Add a Point class demonstrating __new__ and __init__ methods
    Introduce a new Point class to highlight the behavior of `__new__` and `__init__` methods in object creation. Includes a simple example with instance initialization and printing to illustrate the process.

---
+++ course/3.py
@@ -0,0 +1,11 @@
+class Point:
+    def __new__(cls, *args, **kwargs):
+        print('__new__ called' + str(cls))
+
+    def __init__(self, x=0, y=0):
+        print('__init__ called' + str(self))
+        self.x = x
+        self.y = y
+
+pt = Point(1,2)
+print(pt)

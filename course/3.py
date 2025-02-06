8c9480baaadceb4f943f11ee1a839768430917f7
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-06 17:58:35 +0200

    Refactor DataBase class to improve initialization logic
    Updated __init__ to accept user, password, and port as optional arguments, ensuring instance attributes are only set once. Fixed typo in the connect method's output and added example usage at the end for demonstration purposes.

--- course/3.py
+++ course/3.py
@@ -6,13 +6,14 @@ class DataBase:
             cls.__instance = super().__new__(cls)
         return cls.__instance
 
-    def __init__(self):
-        self.user = user()
-        self.psw = psw
-        self.port = port
+    def __init__(self, user=None, psw=None, port=None):
+        if not hasattr(self, 'user'):
+            self.user = user
+            self.psw = psw
+            self.port = port
 
     def connect(self):
-        print(f'Conect with DataBase: user: {self.user} psw: {self.psw} port: {self.port}')
+        print(f'Connect with DataBase: user: {self.user} psw: {self.psw} port: {self.port}')
 
     def close(self):
         print('Close connection')
@@ -20,5 +21,13 @@ class DataBase:
     def read(self):
         print('Read data')
 
-    def write(self):
+    def write(self, data):
         print(f'Write in data {data}')
+
+
+# Приклад використання
+dp = DataBase('root', 'helloworld', 80)
+dp2 = DataBase('root2', 'helloworld1', 40)
+print(id(dp), id(dp2))
+dp.connect()
+dp2.connect()

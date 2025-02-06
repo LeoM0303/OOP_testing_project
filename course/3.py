d92a110090b5a4973c6d00f48e4127740bf92603
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-06 14:45:27 +0200

    Fix missing return in Point's __new__ method
    Previously, the __new__ method did not return an instance, potentially causing issues when creating objects. This change ensures the method properly delegates instance creation to the parent class.

--- course/3.py
+++ course/3.py
@@ -1,6 +1,7 @@
 class Point:
     def __new__(cls, *args, **kwargs):
         print('__new__ called' + str(cls))
+        return super().__new__(cls)
 
     def __init__(self, x=0, y=0):
         print('__init__ called' + str(self))

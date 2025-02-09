74a49cc563c7914404bbc01b1fcbf1fcd27f83d9
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-09 21:48:20 +0200

    Refactor database class initialization and method outputs
    Updated `__init__` to prompt user input instead of passing arguments directly, improving flexibility. Adjusted method outputs for more consistent formatting. Removed unused example code for better clarity and simplicity.

--- course/3.py
+++ course/3.py
@@ -6,28 +6,28 @@ class DataBase:
             cls.__instance = super().__new__(cls)
         return cls.__instance
 
-    def __init__(self, user=None, psw=None, port=None):
-        if not hasattr(self, 'user'):
-            self.user = user
-            self.psw = psw
-            self.port = port
+    def __init__(self, users=None, password=None, port=None):
+        if not hasattr(self, 'users'):
+            self.users = input("Enter users: ")
+            self.password = input("Enter password: ")
+            self.port = input("Enter port: ")
 
     def connect(self):
-        print(f'Connect with DataBase: user: {self.user} psw: {self.psw} port: {self.port}')
+        print(f'Connect to database: users={self.users.capitalize()}, password={self.password}, port={self.port}')
 
     def close(self):
-        print('Close connection')
+        print(f'Close connection')
 
     def read(self):
-        print('Read data')
+        print(f'Read data')
 
-    def write(self, data):
-        print(f'Write in data {data}')
+    def write(self):
+        print(f'Write data')
 
+db = DataBase()
+db2 = DataBase()
 
-# Приклад використання
-dp = DataBase('root', 'helloworld', 80)
-dp2 = DataBase('root2', 'helloworld1', 40)
-print(id(dp), id(dp2))
-dp.connect()
-dp2.connect()
+db.connect()
+db2.connect()
+
+print(id(db), id(db2))

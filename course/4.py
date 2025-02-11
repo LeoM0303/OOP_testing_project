b4595cffb4fdd72a0a80bb6a3fd37df10b5dca21
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-11 18:41:39 +0200

    Set default Vector coordinates to (0, 0) and add validation.
    Default coordinates are now initialized to (0, 0), and validation ensures that only valid coordinates are assigned. This improves data integrity by preventing invalid inputs during object creation.

--- course/4.py
+++ course/4.py
@@ -7,14 +7,13 @@ class Vector:
         return cls.MinCoard <= arg <= cls.MaxCoard
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = self.y = 0
+        if self.get_valid_coards(x) and self.get_valid_coards(y):
+            self.x = x
+            self.y = y
 
     def get_coords(self):
         return self.x, self.y
 
 v = Vector(1,2)
 print(v.get_coords())
-
-print(Vector.get_valid_coards(1))
-print(Vector.get_valid_coards(101))

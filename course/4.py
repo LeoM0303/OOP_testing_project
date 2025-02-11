e68efed066e26787b8ef54baf87765735685dd06
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-11 18:15:34 +0200

    Add Vector class implementation with basic functionality.
    Implemented a Vector class with initialization and coordinate retrieval methods. Includes a simple demonstration of usage and output of vector coordinates.

---
+++ course/4.py
@@ -0,0 +1,10 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_coords(self):
+        return self.x, self.y
+
+v = Vector(1,2)
+print(v.get_coords())

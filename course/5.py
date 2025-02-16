e4c68027145692a192797b38d0ffc9e03c2dda8f
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-16 22:41:22 +0200

    Add Point class definition with initialization logic
    Introduced a new `Point` class in `5.py` with attributes `_x` and `_y`, and a constructor for initialization. Demonstrated usage of the class by creating an instance and attempting to access and modify attributes directly.

--- course/5.py
+++ course/5.py
@@ -1,8 +1,15 @@
 class Point:
     def __init__(self, x=0, y=0):
-        self._x = x
-        self._y = y
+        self.__x = x
+        self.__x = y
+
+    def set_coords(self, x,y):
+        self.__x = x
+        self.__y = y
+
+    def get_coords(self):
+        return self.__x, self.__y
 
 pt = Point(1,2)
-pt.x = 200
-print(pt._x, pt._y)
+pt.set_coords(3,4)
+print(pt.get_coords())

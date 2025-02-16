01450a51ba7aa194ae02d0932e890e0891ddc4ad
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-16 23:07:03 +0200

    Update visibility of __check_coards method to private
    Replaced the use of `protected` with `private` for the `__check_coards` method to ensure stricter encapsulation. This change enhances.

--- course/5.py
+++ course/5.py
@@ -5,17 +5,17 @@ from accessify import private, protected
 class Point:
     def __init__(self, x=0, y=0):
         self.__x = self.__y = 0
-        if self.__check_coards(x) and self.__check_coards(y):
+        if self.check_coards(x) and self.check_coards(y):
             self.__x = x
             self.__x = y
 
     @private
     @classmethod
-    def __check_coards(cls, x):
+    def check_coards(cls, x):
         return type(x) in (int, float)
 
-    def set_coords(self, x,y):
-        if self.__check_coards(x) and self.__check_coards(y):
+    def get_coords(self, x,y):
+        if self.check_coards(x) and self.check_coards(y):
             self.__x = x
             self.__y = y
         else:
@@ -25,5 +25,6 @@ class Point:
         return self.__x, self.__y
 
 pt = Point(1,2)
-pt.set_coords(3,4)
-print(dir(pt))
+pt.get_coords(3,4)
+
+print(pt.get_coords())
